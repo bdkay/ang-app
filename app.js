d102fbacbfaede1395ad603fd5f35d4fad4dc50a
Brendon Kuleck <bkuleck@caxy.com>
2018-05-24 11:38:46 -0500

    Watching the event loop

--- app.js
+++ app.js
@@ -7,19 +7,21 @@
 // }]);
 
 
-// var myApp = angular.module('myApp', []);
-//
-// myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter){
-//
-//   $scope.handle = '';
-//   $scope.lowercasehandle = function(){
-//     return $filter('lowercase')($scope.handle);
-//   };
-//
-// }]);
+var myApp = angular.module('myApp', []);
 
-var tb = document.getElementById("name");
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter){
 
-tb.addEventListener("keypress", function(event){
-  console.log("Pressed!");
-});
+  $scope.handle = '';
+  $scope.lowercasehandle = function(){
+    return $filter('lowercase')($scope.handle);
+  };
+
+  $scope.$watch('handle', function(newValue, oldValue){
+
+    console.info('Changed!');
+    console.log('Old ' + oldValue);
+    console.log('New  ' + newValue);
+
+  });
+
+}]);

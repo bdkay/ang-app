8ea6f19781f453f0f4ee9afc052d811c7f1885b5
Brendon Kuleck <bkuleck@caxy.com>
2018-05-25 11:43:30 -0500

    Clean sandbox

--- app.js
+++ app.js
@@ -11,51 +11,6 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('mainController', ['$scope', '$filter', '$http', function($scope, $filter, $http){
 
-  $scope.handle = '';
-
-  $scope.lowercasehandle = function(){
-    return $filter('lowercase')($scope.handle);
-  };
-
-  $scope.characters = 5;
-
-  //result == data that comes back
-  $http.get('/api')
-    .success(function(result){
-
-      $scope.rules = result;
-
-    })
-    .error(function(data, status){
-
-      console.log(data);
-
-    });
-
-  $scope.newRule = '';
-
-  //Passing JSON to api
-  $http.post('/api', { newRule: $scope.newRule })
-    .success(function(result){
-      $scope.rules = result;
-      $scope.newRule = '';
-    })
-    .error(function(data, status){
-      console.log(data);
-    })
-
-  //
-  // //Common directives
-  //
-  // $scope.characters = 5;
-  // $scope.rules = [
-  //
-  //   { rulename: "Must be 5 characters" },
-  //   { rulename: "Must not be used elsewhere"},
-  //   { rulename: "Must be cool" }
-  //
-  // ];
-  //
-  // console.log($scope.rules);
+  
 
 }]);

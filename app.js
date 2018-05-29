e666ec6df56f2f782469ddc98ce99ead7279cb02
Brendon Kuleck <bkuleck@caxy.com>
2018-05-29 14:50:16 -0500

    Directives and two-way data binding

--- app.js
+++ app.js
@@ -7,15 +7,55 @@
 // }]);
 
 
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.controller('mainController', ['$scope', function($scope){
+myApp.config(function($routeProvider){
+
+  $routeProvider
+  .when('/main', {
+    templateUrl: 'pages/main.html',
+    controller: 'mainController'
+  })
+  .when('/second', {
+    templateUrl: 'pages/second.html',
+    controller: 'secondController'
+  })
+
+});
+
+myApp.service('nameService', function(){
+
+  var self = this;
+  this.name = 'Brendon Kuleck';
+  this.nameLength = function(){
+
+    return self.name.length;
+
+  }
+
+});
+
+myApp.controller('mainController', ['$scope', '$log', 'nameService', function($scope, $log, nameService){
+
+  $scope.person = {
+    name: 'Brendon Kuleck',
+    address: '2618 W. Chicago Ave, Chicago, IL'
+  }
+
+}]);
+
+myApp.controller('secondController', ['$scope', '$log', '$routeParams', 'nameService', function($scope, $log, $routeParams, nameService){
 
 
 }]);
 
-myApp.controller('secondController', ['$scope', function($scope){
-
-
-
-}]);
+myApp.directive('searchResult', function() {
+  return {
+    templateUrl: 'directives/searchresult.html',
+    replace: true,
+    scope: {
+      //Equals sign = is two-way-binding
+      personObject: '='
+    }
+  }
+});
